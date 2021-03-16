7e2f7c86d175cee7be95a8a51e0b71fe6ed53f32
Bartolomeu <goncalo@example.com>
2021-03-16 22:13:52 +0000

    Tpc1

---
+++ tpc1 - managed-static-link/PointApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PointApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointLib.Point p = new PointLib.Point(3,7);
+
+            Console.WriteLine(p.toString());
+
+            Console.WriteLine("p._x = " + p.x);
+
+            Console.WriteLine("p._y = " + p.y);
+        }
+    }
+}
---
+++ tpc1 - managed-static-link/PointLib/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PointLib{
+    public class Point{
+
+        public double x, y;
+
+        public Point(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getModule(){
+            return Math.Sqrt(x*x + y*y);
+        }
+
+        public string toString(){
+            return "Point (x = " + x + ", y = " + y + ")";
+        }
+    }
+}

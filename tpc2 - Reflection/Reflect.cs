479f127cd94c8f13e4caa42b6697047546193ead
Bartolomeu <goncalo@example.com>
2021-03-21 22:09:03 +0000

    tpc 2 Reflection

---
+++ tpc2 - Reflection/Reflect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace tpc2___Reflection
+{
+    class REflect
+    {
+        static void Main(string[] args)
+        {
+
+            Assembly a = Assembly.Load ( "RestSharp" );
+            Type[] types = a.GetTypes();
+
+            foreach(var t in types){
+                Console.WriteLine(t.Name);
+
+                MethodInfo[] methods = t.GetMethods();
+
+                foreach(var curr in methods){
+                    Console.WriteLine("\t" + curr.Name);
+                }
+            }
+            
+        }
+    }
+}

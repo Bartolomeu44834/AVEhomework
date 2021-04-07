4764454369e5d210144ecacac99ec595c2c4f8db
Bartolomeu <goncalo@example.com>
2021-04-07 16:14:46 +0100

    tpc4 feito

---
+++ tpc4 - FireStore/AppFireStudents/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Cloud.Firestore;
+
+namespace AppFireStudents
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FirestoreDb db = FirestoreDb.Create("fire-students-3edf2");
+            QuerySnapshot query = db.Collection("students").GetSnapshotAsync().Result;
+
+            foreach(DocumentSnapshot doc in query){
+                Console.Write('{');
+                Dictionary<string,object> map = doc.ToDictionary();
+                foreach(var pair in map){
+                    Console.Write($"{pair.Key} : {pair.Value},");
+                }
+                Console.Write("\b");
+                Console.WriteLine("}");
+            }
+        }
+    }
+}
